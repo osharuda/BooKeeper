8e4955e2835eab4d3e38bd8cc389258438328af2
osharuda <osharuda@github.com>
2025-01-07 18:07:46 +0100

    * Minor fixes

--- docbrowser.py
+++ docbrowser.py
@@ -1,9 +1,22 @@
+"""
+    Copyright 2025 Oleh Sharuda <oleh.sharuda@gmail.com>
+
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ """
+
 from collections.abc import Callable
 from enum import IntEnum
-from inspect import Attribute
-from sys import flags
-
-from imgui import menu_item
 from imgui.integrations.glfw import GlfwRenderer
 import OpenGL.GL as gl
 import glfw
